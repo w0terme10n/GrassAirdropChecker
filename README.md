10c6064039b0aeab068b07fff16b0e2632ce6c0b
Egor <egorgolev702@gmail.com>
2024-09-08 00:25:31 +0300

    add proxies, total points, disable ssl (https) connection

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 ## Launching
 1) Install python and requirements (pip install -r requirements.txt)
-2) Put your wallets to wallets.txt
-3) Check config.py for requests
-4) python main.py
+2) Put your wallets in the wallets.txt
+3) Put proxies in the proxies.txt (in format: socks5://user:password@ip:port or http://user:password@ip:port)
+4) Check config.py for requests
+5) python main.py
 
 Results will be in results.txt
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from config import REQUESTS_AT_ONCE
 from tqdm import tqdm
 import aiohttp
 import asyncio
+import random
 
 
 def split_list_by_length(lst: list, length: int):
@@ -17,29 +18,33 @@ def split_list_by_length(lst: list, length: int):
     return result
 
 
-async def fetch_earnings(session: aiohttp.ClientSession, wallet: str) -> dict:
+async def fetch_earnings(session: aiohttp.ClientSession, wallet: str, proxy: str) -> dict:
     url = f'https://api.getgrass.io/airdropAllocations?input=%7B%22walletAddress%22:%22{wallet}%22%7D'
     points = 0
     try:
-        async with session.get(url) as response:
+        async with session.get(url, verify_ssl=False, proxy=proxy) as response:
             if response.status == 200:
                 data = await response.json()
                 if 'result' in data and 'data' in data['result']:
                     for epoch in data['result']['data']:
                         points += data['result']['data'][epoch]
+                    points = round(points, 2)
     except Exception as e:
-        print(f'error while request at wallet {wallet}\n{e}')
+        print(f'error while request at wallet {wallet} with {proxy}\n{e}')
         points = 'undefined'
     return {'wallet': wallet, 'points': points}
 
 
-async def process_requests(wallets: list[str]) -> list[dict]:
+async def process_requests(wallets: list[str], proxies: list[str]) -> list[dict]:
     splitted_wallets = split_list_by_length(wallets, REQUESTS_AT_ONCE)
     result = []
     for i in tqdm(range(len(splitted_wallets))):
-        async with aiohttp.ClientSession() as session:
-            tasks = [fetch_earnings(session, wallet) for wallet in splitted_wallets[i]]
-            result += await asyncio.gather(*tasks)
+        try:
+            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False, use_dns_cache=False)) as session:
+                tasks = [fetch_earnings(session, wallet, random.choice(proxies)) for wallet in splitted_wallets[i]]
+                result += await asyncio.gather(*tasks)
+        except Exception as e:
+            print(f'Error while {i+1} request\n{e}')
         await asyncio.sleep(0.2)
     return result
 
@@ -47,11 +52,18 @@ async def process_requests(wallets: list[str]) -> list[dict]:
 def main():
     with open('wallets.txt') as f:
         wallets = [i.strip() for i in f.readlines() if i.strip()]
+
+    with open('proxies.txt') as f:
+        proxies = [i.strip() for i in f.readlines() if i.strip()]
     
-    result = asyncio.run(process_requests(wallets))
+    result = asyncio.run(process_requests(wallets, proxies))
+    total = 0
     with open('results.txt', 'w') as f:
         for row in result:
+            if isinstance(row['points'], float) or isinstance(row['points'], int):
+                total += row["points"]
             f.write(f'{row["wallet"]} - {row["points"]} tokens\n')
+        f.write(f'total: {total} tokens\n')
 
 
 if __name__ == '__main__':
